b5a5ccb88b40e5b29116ae939ca2714adb485380
Preston Whitehall <pmwhitehall@gmail.com>
2019-09-30 20:09:14 -0700

    Adds props and dynamic rendering of props to List

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -1,21 +1,26 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/require-default-props */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import PropTypes from 'prop-types';
 import ListItem from './ListItem';
+import gamesProp from '../../types/index';
 
 const List = (props) => {
-  const { items } = props;
+  const { games, clickAddToLibrary } = props;
   return (
     <div>
       <h4> List Component </h4>
-      There are { items.length } items.
-      { items.map((item) => <ListItem item={item} />)}
+      There are { games.length } games.
+      <div id="games-container">
+        { games.map((game) => <ListItem key={game.id} game={game} clickAddToLibrary={clickAddToLibrary} />)}
+      </div>
     </div>
   );
 };
 
 List.propTypes = {
-  items: PropTypes.string.isRequired,
+  games: PropTypes.arrayOf(gamesProp),
 };
 
 export default List;
