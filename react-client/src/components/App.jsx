d28887c16707db67833dcc05bcd34b167a287e3e
Preston Whitehall <pmwhitehall@gmail.com>
2019-09-30 20:07:48 -0700

    Adds search handling, click framework, dynamic state handling

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -1,30 +1,84 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-nested-ternary */
 /* eslint-disable no-console */
 import React from 'react';
 import axios from 'axios';
 import List from './List';
+import Search from './Search';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [],
+      library: [],
+      wishlist: [],
+      searchResults: [],
+      view: 'library',
     };
+    this.getSearchResults = this.getSearchResults.bind(this);
+    this.clickAddToLibrary = this.clickAddToLibrary.bind(this);
+    this.clickAddToWishList = this.clickAddToWishList.bind(this);
   }
 
   componentDidMount() {
-    axios.get('/items')
-      .then((items) => {
-        this.setState({ items });
+    axios.get('/library')
+      .then((response) => {
+        const library = response.data;
+        this.setState({ library });
       })
       .catch((error) => console.log(error));
   }
 
+  getSearchResults(searchTerm) {
+    console.log('APP', searchTerm);
+    axios.get(`/api/search/${searchTerm}`)
+      .then((response) => {
+        const searchResults = response.data;
+        this.setState({ searchResults });
+      });
+  }
+
+  clickAddToLibrary(game) {
+    // do stuff
+    axios.post('/api/library/addGame', game)
+      .then((response) => {
+        // do some stuff with the response
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  clickAddToWishList() {
+    // do other stuff
+    axios.post('/api/wishlist/addGame')
+      .then((response) => {
+        // do some stuff with the response
+      });
+  }
+
+  // How to render three views?
+  // start on search page
   render() {
-    const { items } = this.state;
+    const {
+      library,
+      wishlist,
+      searchResults,
+      view,
+    } = this.state;
+    let display = library;
+    if (view === 'wishlist') {
+      display = wishlist;
+    } else if (view === 'searchResults') {
+      display = searchResults;
+    }
+    console.log(library);
+    // const display = games;
     return (
       <div>
-        <h1>Item List</h1>
-        <List items={items} />
+        <h1>Bloop</h1>
+        <Search getSearchResults={this.getSearchResults} />
+        <List games={display} clickAddToLibrary={this.clickAddToLibrary} />
       </div>
     );
   }
