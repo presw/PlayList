011079afd2c71d4a976c558ee1272237b98d350e
Preston Whitehall <pmwhitehall@gmail.com>
2019-12-07 09:43:43 -0800

    Corrects destructuring error

--- database/index.js
+++ database/index.js
@@ -1,7 +1,12 @@
 const Pool = require('pg');
 const config = require('../config');
 
-const [user, host, database, password] = config;
+const {
+  user,
+  host,
+  database,
+  password,
+} = config;
 
 const pool = new Pool({
   user,
